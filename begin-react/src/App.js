1724d4dfaca32f006d7c5bab4d23d259677bac98
Kjintae <wlsxo2112@naver.com>
2020-05-08 17:36:59 +0900

    component , jsx basic

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Hello from './Hello'; // hello 컴포넌트를 불러오겠다.
 import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fonotSize: 24,
+    padding: '1rem'
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello />
+      <div style={style}>{name}</div> 
+      <div className="gray-box"></div>
     </div>
   );
 }
 
 export default App;
+// 태그는 꼭 닫혀야 한다
+// 두개이상의 태그는 하나의 태그로 감싸줘야한다. <> 프래그먼트? 로 감싸면된다.
+// 자바스크립트 값을 jsx내부에서 보여줄때는 {}로 감싸서 해야한다.
+// jsx내부에서 스타일을 사용하고 싶을 때는 객체형태로 사용하여야 한다.
+// class가 아니고 classNamed이다.
---
+++ begin-react/src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+// component 이름은 대문자
+function Hello() {
+    return <div>안녕하세요</div>;
+}
+
+export default Hello; // 컴포넌트를 내보내겠다.
